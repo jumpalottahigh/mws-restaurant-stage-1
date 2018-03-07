e691b6e0a07b301875a42fc28ba39b61e9b699eb
Georgi Yanev <georgiyanev.gy@gmail.com>
2018-03-07 17:04:32 +0200

    Create easy deployment with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,55 @@
-var gulp = require('gulp');
-// var copy = require('gulp-contrib-copy');
+const gulp = require('gulp')
+const deploy = require('gulp-gh-pages')
 
-var ghPages = require('gulp-gh-pages');
+// Prep HTML to build
+gulp.task('copyHTML', function() {
+  return gulp
+    .src([
+      'index.html',
+      'restaurant.html'
+    ])
+    .pipe(gulp.dest('build/'))
+})
 
-gulp.task('deploy', function() {
-  return gulp.src('./build/**/*').pipe(ghPages());
-});
+// Prep JS to build
+gulp.task('copyJS', function() {
+  return gulp
+    .src([
+      'js/**/*'
+    ])
+    .pipe(gulp.dest('build/js/'))
+})
 
-// gulp.task('default', copy);
+// Prep CSS to build
+gulp.task('copyCSS', function() {
+  return gulp
+    .src([
+      'css/**/*'
+    ])
+    .pipe(gulp.dest('build/css/'))
+})
+
+// Prep data to build
+gulp.task('copyData', function() {
+  return gulp
+    .src([
+      'data/**/*'
+    ])
+    .pipe(gulp.dest('build/data/'))
+})
+
+// Prep images to build
+gulp.task('copyImg', function() {
+  return gulp
+    .src([
+      'img/**/*'
+    ])
+    .pipe(gulp.dest('build/img/'))
+})
+
+gulp.task('deploy', ['copyHTML', 'copyJS', 'copyCSS', 'copyData', 'copyImg'], function() {
+  return gulp.src(['./build/**/*']).pipe(deploy())
+})
+
+// Explicitly specify default task
+gulp.task('default', ['copyHTML', 'copyJS', 'copyCSS', 'copyData', 'copyImg'])
