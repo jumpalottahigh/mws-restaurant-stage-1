55a6d9f09464db6d5a838a6bcdd33e019b1440d1
Georgi Yanev <georgiyanev.gy@gmail.com>
2018-03-07 17:08:38 +0200

    Update 2018-03-07T15:08:37.045Z

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -7,8 +7,10 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 4444; // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    // const port = 4444; // Change this to your server port
+    // return `http://localhost:${port}/data/restaurants.json`;
+    //Production URL
+    return 'https://jumpalottahigh.github.io/mws-restaurant-stage-1/data/restaurants.json'
   }
 
   /**
