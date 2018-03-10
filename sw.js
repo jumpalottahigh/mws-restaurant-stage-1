e4c8a761086a275a8920a06fb0f1cf3cb22b0ebb
Georgi Yanev <georgiyanev.gy@gmail.com>
2018-03-10 23:48:41 +0200

    Cache app images aggressively

--- sw.js
+++ sw.js
@@ -1,4 +1,67 @@
-const currentCacheVersion = 'mws-restaurant-v14';
+const currentCacheVersion = 'mws-restaurant-v22';
+const imagesToCache = [
+  '/img/1.webp',
+  '/img/2.webp',
+  '/img/3.webp',
+  '/img/4.webp',
+  '/img/5.webp',
+  '/img/6.webp',
+  '/img/7.webp',
+  '/img/8.webp',
+  '/img/9.webp',
+  '/img/10.webp',
+  '/img/1_400.webp',
+  '/img/2_400.webp',
+  '/img/3_400.webp',
+  '/img/4_400.webp',
+  '/img/5_400.webp',
+  '/img/6_400.webp',
+  '/img/7_400.webp',
+  '/img/8_400.webp',
+  '/img/9_400.webp',
+  '/img/10_400.webp',
+  '/img/1_800.webp',
+  '/img/2_800.webp',
+  '/img/3_800.webp',
+  '/img/4_800.webp',
+  '/img/5_800.webp',
+  '/img/6_800.webp',
+  '/img/7_800.webp',
+  '/img/8_800.webp',
+  '/img/9_800.webp',
+  '/img/10_800.webp',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
+  '/img/1_400.jpg',
+  '/img/2_400.jpg',
+  '/img/3_400.jpg',
+  '/img/4_400.jpg',
+  '/img/5_400.jpg',
+  '/img/6_400.jpg',
+  '/img/7_400.jpg',
+  '/img/8_400.jpg',
+  '/img/9_400.jpg',
+  '/img/10_400.jpg',
+  '/img/1_800.jpg',
+  '/img/2_800.jpg',
+  '/img/3_800.jpg',
+  '/img/4_800.jpg',
+  '/img/5_800.jpg',
+  '/img/6_800.jpg',
+  '/img/7_800.jpg',
+  '/img/8_800.jpg',
+  '/img/9_800.jpg',
+  '/img/10_800.jpg'
+];
+
 const resourcesToCache = [
   '/',
   '/index.html',
@@ -7,9 +70,8 @@ const resourcesToCache = [
   '/js/dbhelper.js',
   '/js/main.js',
   '/js/restaurant_info.js',
-  '/data/restaurants.json'
-  // 'https://normalize-css.googlecode.com/svn/trunk/normalize.css',
-  // 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD_S9Z-6zktuRp_GiEL2iTXUAKf_q-p7UY&libraries=places&callback=initMap',
+  '/data/restaurants.json',
+  ...imagesToCache
 ];
 
 // Install SW
@@ -53,7 +115,18 @@ self.addEventListener('fetch', event => {
         if (response) return response;
 
         // Otherwise hit the network
-        return fetch(event.request, { mode: 'no-cors' }).then(netResponse => {
+        return fetch(event.request).then(netResponse => {
+          // Only cache images from the app
+          if (
+            netResponse.url.includes('.jpg') ||
+            netResponse.url.includes('.webp')
+          ) {
+            if (netResponse.url.includes(window.location.origin)) {
+              cache.put(event.request.url, netResponse.clone());
+              return netResponse;
+            }
+            return;
+          }
           console.log(netResponse);
           cache.put(event.request.url, netResponse.clone());
           return netResponse;
